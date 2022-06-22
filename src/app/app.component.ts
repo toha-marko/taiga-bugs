35208ba86b99389df9be5560de8e8f8872e64205
Anton Marinenko <aamarinenko@tsum.ru>
2022-06-22 23:31:49 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { BehaviorSubject, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'taiga-bugs';
+
+  columns$ = new BehaviorSubject(['title', 'id']);
+
+  headings$ = new BehaviorSubject({
+    title: 'Название',
+    id: 'ID',
+    prop: 'Проперти'
+  });
+
+  data$ = of([
+    {
+      id: '1',
+      title: 'title1',
+      prop: 'some prop'
+    },
+    {
+      id: '1',
+      title: 'title1',
+      prop: 'some prop'
+    }
+  ])
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.columns$.next(['title', 'id', 'prop'])
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TuiTableModule } from "@taiga-ui/addon-table";
+import { TuiLetModule } from "@taiga-ui/cdk";
+
+const TUI_MODULES = [
+  TuiTableModule,
+  TuiLetModule,
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
-  providers: [],
+    BrowserModule,
+      TuiRootModule,
+      BrowserAnimationsModule,
+      TuiDialogModule,
+    TuiAlertModule,
+    TUI_MODULES,
+],
+  providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
